4611ffee565df6b0c7f0a6780640cd791c8521c3
boyninja15 <flappyfloorg@gmail.com>
2025-06-14 10:16:30 -0500

    GitHub Pages support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@astrojs/react";
 export default defineConfig({
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ]
-  },
-  integrations: [
-    react(),
-  ],
+	site: "https://tll-web.flappygrant.com",
+	vite: {
+		plugins: [
+			tailwindcss(),
+		]
+	},
+	integrations: [
+		react(),
+	],
 })
